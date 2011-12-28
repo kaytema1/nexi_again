1a5cbef7e8f6c8171041a4be379eeb7840c8c14d
Emmanuel Addo-Yirenkyi <emmanuel@ubuntu.ubuntu-domain>
2011-12-28 14:14:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nexiweb::Application.config.secret_token = 'b31e77c045f39f9f03fba5047f3fa7eb073dbabce6fd08629e4ea390fa8a765bbdf6b8ad5788e885d7c7fe1ced4b381efcadd7187f51f9c863f958b1b74755aa'
